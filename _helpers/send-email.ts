bfe680c5d3636a2e6fc306a9871fd2f6454a91d5
unknown <jericsolitana>
2026-05-01 19:57:25 +0800

    updated commit

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,7 +1,13 @@
 import nodemailer from 'nodemailer';
 import config from '../config.json';
 
-export default async function sendEmail({to, subject, html, from = config.emailFrom}: any) {
+export default async function sendEmail({ to, subject, html, from = config.emailFrom }: any) {
     const transporter = nodemailer.createTransport(config.smtpOptions);
-    await   transporter.sendMail({ from, to, subject, html });
+
+    // SEND EMAIL
+    const info = await transporter.sendMail({ from, to, subject, html });
+
+    // ✅ ADD THESE LINES
+    console.log("Message sent:", info.messageId);
+    console.log("Preview URL:", nodemailer.getTestMessageUrl(info));
 }
--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -1,10 +1,15 @@
-import express from 'express';
+import express from "express";
+import swaggerUi from "swagger-ui-express";
+import YAML from "yamljs";
+import path from "path";
+
 const router = express.Router();
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';  
 
-const swaggerDocument = YAML.load('./swagger.yaml');
+// FIXED PATH
+const swaggerDocument = YAML.load(
+  path.join(__dirname, "../swagger.yaml")
+);
 
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default router;
