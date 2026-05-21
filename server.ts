34c809912dfbd942933042cdc0fea664d64594e4
unknown <jericsolitana>
2026-05-21 10:24:27 +0800

    updated server.ts nasad

--- server.ts
+++ server.ts
@@ -21,7 +21,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // handle preflight
+app.options('/*', cors(corsOptions)); // handle preflight
 
 // api routes
 app.use('/accounts', accountsController);
