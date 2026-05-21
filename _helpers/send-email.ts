c3315db88011abbf6a603a8077b6c2af492e6c0f
unknown <jericsolitana>
2026-05-21 11:17:46 +0800

    updated send-email.tshuhu

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -2,14 +2,14 @@ import nodemailer from 'nodemailer';
 
 export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
     const transporter = nodemailer.createTransport({
-        host: process.env.SMTP_HOST,
-        port: Number(process.env.SMTP_PORT),
-        secure: true, // true for port 465
-        auth: {
-            user: process.env.SMTP_USER,
-            pass: process.env.SMTP_PASS
-        }
-    });
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false,
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+});
 
     const info = await transporter.sendMail({ from, to, subject, html });
     console.log("Message sent:", info.messageId);
